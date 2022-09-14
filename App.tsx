d54139f0eda91b7221603239ed0e67587836bfca
Lucas Castilho <lucas.h.a.oliveira@accenture.com>
2022-09-14 10:59:25 -0300

    config eslint

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import React from "react"
-import { Text } from "react-native"
+import React from 'react';
+import {Text} from 'react-native';
 
 const App = () => {
-  return <Text>Hello Vini</Text>
-}
+  return <Text>Hello Vini</Text>;
+};
 
-export default App
+export default App;
